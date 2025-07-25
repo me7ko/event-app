f497282a100823e1aaac86639324c105be4a9d64
metko61 <metko61@gmail.com>
2025-07-25 20:45:33 +0300

    Update knexfile to use environment variables

--- knexfile.js
+++ knexfile.js
@@ -9,11 +9,11 @@ module.exports = {
   development: {
     client: "pg",
     connection: {
-      host: process.env.DB_HOST || "localhost",
-      port: process.env.DB_PORT || 5433,
-      database: process.env.DB_NAME || "eventapp",
-      user: process.env.DB_USER || "postgres",
-      password: process.env.DB_PASSWORD || "yourpassword",
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
     },
     migrations: {
       directory: "./migrations",
