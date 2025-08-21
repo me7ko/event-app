bb0d781128831669dfe1f1e3b4506c0efe7e4411
metko61 <metko61@gmail.com>
2025-08-21 19:21:28 +0300

    Fixed Footer

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,3 +1,4 @@
+// app/layout.js
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
@@ -5,11 +6,7 @@ import Footer from "./components/Footer";
 import { AuthProvider } from "./context/AuthContext";
 import { ToastProvider } from "./context/ToastContext";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
+const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
@@ -22,13 +19,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" className="h-full">
+      <body className="min-h-screen h-full flex flex-col">
         <AuthProvider>
           <ToastProvider>
-            {" "}
             <Navbar />
-            {children}
+            <main className="flex-1">{children}</main>
             <Footer />
           </ToastProvider>
         </AuthProvider>
